79e1b19cea936ff11ddf785e14bfa3fd5ce59ab1
55555 <glinskaya33@mail.ru>
2022-11-20 22:26:36 +0300

    OOPHW1Part1

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,16 @@
+
 public class Person {
-}
+    String name;
+    int yearOfBirth;
+    String town;
+    String jobTitle;
+
+    Person(String name, String town, int yearOfBirth,String jobTitle) {
+        this.name = name;
+        this.town = town;
+        this.yearOfBirth = yearOfBirth;
+        this.jobTitle = jobTitle;
+    }
+    void getAcquainted(){
+        System.out.println("Привет меня зовут " + name + " Я из города " + town + " Я родился в " + yearOfBirth + " году " + " Я работаю на должности " + jobTitle + " Будем знакомы!");
+    }}
